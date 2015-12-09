5bdcc7589634346d41906e7b2a89898d661fc3c4
Nathan Park <email": null>
2015-12-09 10:53:05 -0800

    refactor .on miniQuery function, create listener for list of students

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 $.ready(function() {
   studentList();
+  studentBadges();
 
-  $('ul').on('click', 'li', function(event){
-    event.preventDefault();
-    debugger
-  });
 });
 
 
@@ -19,25 +16,30 @@ var studentList = function(){
     var students = JSON.parse(response)
     var data = {'students': students}
     var result = template(data)
-    $('#studentList').append(result)
+    $('#studentList').html(result)
   }).catch(function(error){
     console.log(error);
   });
 };
 
 var studentBadges = function(){
-  var request = $.ajax({
-    url: 'http://localhost:3000/students',
-    type: 'get',
-    dataType: 'json'
-  }).then(function(response){
-    var source = $('#student-names').html()
-    var template = Handlebars.compile(source)
-    var students = JSON.parse(response)
-    var data = {'students': students}
-    var result = template(data)
-    $('#studentList').append(result)
-  }).catch(function(error){
-    console.log(error);
+  $('ul').on('click', function(event){
+    event.preventDefault();
+    debugger
+
+    var request = $.ajax({
+      url: 'http://localhost:3000/students/:id',
+      type: 'get',
+      dataType: 'json'
+    }).then(function(response){
+      var source = $('#student-names').html()
+      var template = Handlebars.compile(source)
+      var students = JSON.parse(response)
+      var data = {'students': students}
+      var result = template(data)
+      $('#studentList').append(result)
+    }).catch(function(error){
+      console.log(error);
+    });
   });
 };
--- js/miniQuery.js
+++ js/miniQuery.js
@@ -72,11 +72,15 @@ function miniQuery(selector){
 
   element.on = function(event, action) {
     if( selector.charAt(0) === '#' ){
-      element.addEventListener(event, action);
+      element.addEventListener(event, function(e){
+        return action(e)
+      });
     }
     else {
       for( var i = 0; i < element.length; i++ ){
-        element[i].addEventListener(event, action);
+        element[i].addEventListener(event, function(e){
+          return action(e)
+        });
       }
     }
   };
