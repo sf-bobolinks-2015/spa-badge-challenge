5471f6e10cc4d9f888c3b936791a483a5ad96141
Jenna Andersen & Mary Engelbrecht <pair+jnicholea.maryengelbrecht@devbootcamp.com>
2015-12-08 19:17:29 -0800

    add ajax request to return and append nominees

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :badges, except: [:new, :edit]
+  resources :nominees
+  resources :votes
   root 'badges#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -9,12 +11,13 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get '/nominees' => 'nominees#new'
-  post '/nominees' => 'nominees#create'
+  # get '/nominees/new' => 'nominees#new'
+  # post '/nominees' => 'nominees#create'
+  # get '/nominees' => 'nominees#index'
 
-  get '/votes' => 'votes#new'
-  post '/votes' => 'votes#create'
-  put '/votes' => 'votes#update'
+  # get '/votes/new' => 'votes#new'
+  # post '/votes' => 'votes#create'
+  # put '/votes' => 'votes#update'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
