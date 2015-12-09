531c6c60efaecad94950650d17e7b306fa47ff31
Eunice Choi & Rosslyn Sinclair-Chin <pair+euniceschoi.rsinclairchin@devbootcamp.com>
2015-12-09 11:33:37 -0800

    Handlebars for displaying name and badges works

--- boot-badge/app/controllers/students_controller.rb
+++ boot-badge/app/controllers/students_controller.rb
@@ -8,7 +8,9 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
     @student_badges = @student.badges
-    render json: @student_badges
+    render json: {
+      :badges => @student_badges,
+      :student => @student}
   end
 
 end
